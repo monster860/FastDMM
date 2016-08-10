1c222e35ee67824d0f08d52b6d92f98fec51b0a3
Pieter Vlasblom <vlasblom.p@outlook.com>
2016-08-10 20:28:46 +0200

    Implement cross-platform file seperator normalization.

--- src/main/java/com/github/monster860/fastdmm/Util.java
+++ src/main/java/com/github/monster860/fastdmm/Util.java
@@ -1,10 +1,29 @@
 package com.github.monster860.fastdmm;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * This class contains various utilities.
+ */
 public class Util {
+
+    /**
+     * The Unix separator character.
+     */
+    private static final char UNIX_SEPARATOR = '/';
+
+    /**
+     * The Windows separator character.
+     */
+    private static final char WINDOWS_SEPARATOR = '\\';
+
+    /**
+     * The system separator character.
+     */
+    private static final char SYSTEM_SEPARATOR = File.separatorChar;
 
     /**
      * Attempts to retrieve a {@link InputStream} representation of a file.
@@ -36,6 +55,58 @@ public class Util {
             return output.toByteArray();
         }
         return null;
+    }
+
+    /**
+     * Determines if Windows file system is in use.
+     *
+     * @return true if the system is Windows
+     */
+    public static boolean isWindowsSystem() {
+        return SYSTEM_SEPARATOR == WINDOWS_SEPARATOR;
+    }
+
+    /**
+     * Converts all separators to the Unix separator of forward slash.
+     *
+     * @param path the path to be changed, null ignored
+     * @return the updated path
+     */
+    public static String separatorsToUnix(String path) {
+        if (path == null || path.indexOf(WINDOWS_SEPARATOR) == -1) {
+            return path;
+        }
+        return path.replace(WINDOWS_SEPARATOR, UNIX_SEPARATOR);
+    }
+
+    /**
+     * Converts all separators to the Windows separator of backslash.
+     *
+     * @param path the path to be changed, null ignored
+     * @return the updated path
+     */
+    public static String separatorsToWindows(String path) {
+        if (path == null || path.indexOf(UNIX_SEPARATOR) == -1) {
+            return path;
+        }
+        return path.replace(UNIX_SEPARATOR, WINDOWS_SEPARATOR);
+    }
+
+    /**
+     * Converts all separators to the system separator.
+     *
+     * @param path the path to be changed, null ignored
+     * @return the updated path
+     */
+    public static String separatorsToSystem(String path) {
+        if (path == null) {
+            return null;
+        }
+        if (isWindowsSystem()) {
+            return separatorsToWindows(path);
+        } else {
+            return separatorsToUnix(path);
+        }
     }
 
 }
